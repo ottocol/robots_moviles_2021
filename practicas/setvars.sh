0e7aec3576c5f0d1a1cb18b7fcf5efa43d2749a9
Otto Colomina <otto@dccia.ua.es>
2020-12-01 13:45:49 +0100

    añadido setvars.sh

---
+++ practicas/setvars.sh
@@ -0,0 +1,13 @@
+#solo vale en los PCs del laboratorio Y con los pendrives de ASUS
+if [ $# -ne 1 ]
+  then
+    echo "argumentos: numero_turtlebot"
+  else
+    mi_ip=$(ip -o -4 addr list | grep "wlx" | awk '{print $4}' | cut -d/ -f1)
+    let offset=$1+4
+    ip_turtlebot=192.168.1.$offset
+    export ROS_MASTER_URI=http://$ip_turtlebot:11311
+    export ROS_HOSTNAME=$mi_ip    
+fi
+
+
